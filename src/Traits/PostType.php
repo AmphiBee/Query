2f338ef5103b90c40981a06faf45b52d4597e3dc
Olivier Gorzalka <olivier@amphibee.fr>
2024-02-25 15:09:30 +0100

    Corrected default post type

--- src/Traits/PostType.php
+++ src/Traits/PostType.php
@@ -6,7 +6,7 @@ namespace Pollen\Query\Traits;
 
 trait PostType
 {
-    protected string $postType = 'all';
+    protected string $postType = 'any';
 
     public function postType(string|array $postType): self
     {
